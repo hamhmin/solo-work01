2b5824ff4596a3fb7fd9ceca938aad2044e95842
hamhmin <49775311+hamhmin@users.noreply.github.com>
2022-08-15 23:31:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,5 +11,29 @@ $(function(){
     $('.lang-box').click(function(){
         $('.lang-box ul').stop().slideToggle(300);
         });
-        
+
+    // 메인비주얼 페이드효과
+        $('.fade-banner').find('img:gt(0)').hide();
+        let current = 0;
+        // setInterval(function(){
+        // let next = (current + 1) % 3;
+
+        // $('.fade-banner').find('img').eq(current).fadeOut(200);
+        // $('.fade-banner').find('img').eq(next).fadeIn(400);
+        // console.log(current, next);
+        // current = next;
+        // },3000);
+
+        $(function(){
+            $(window).scroll(function(){
+                let scrollPosition = $(window).scrollTop();
+                console.log(scrollPosition);
+                if( scrollPosition >= 100 ){
+                   $('.header-slide').stop().fadeIn(300);
+                } else {
+                    $('.header-slide').stop().fadeOut(300);
+                }
+            });
+        });
     });
+
