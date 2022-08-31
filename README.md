f2439d448d9a7246a2fad2b639d6549547bb5354
hamhmin <49775311+hamhmin@users.noreply.github.com>
2022-08-31 10:37:06 +0900

    Add files via upload
    fabicon, 사이트 썸네일 추가 , 헤더 슬라이드 스크롤 이벤트 추가

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ https://hamhmin.github.io/solo-work01/
 
 ## 개요
     에이티 컴퍼니 반응형 웹사이트 메인페이지 입니다.
-    메인 비주얼 이미지 3개를 제외한 비주얼은 직접 제작한 디자인&퍼블리싱 포트폴리오입니다.
 
 ## 작업 내용
 1. 에이티 주식회사 브랜딩 아이콘 제작(figma)
@@ -18,7 +17,6 @@ https://hamhmin.github.io/solo-work01/
     - scroll-behavior: smooth
 8.  배너 이미지 스크롤 고정
     - background-attachment: fixed;
-9. iframe 디지털 시계앱 추가
 
 ## 라이브러리
 - 제이쿼리
--- js/main.js
+++ js/main.js
@@ -1,39 +1,52 @@
-$(function(){
+$(function () {
     // gnb 메뉴 토글
-    $('.gnb-btn').click(function(){
+    $('.gnb-btn').click(function () {
         $('.mobile-nav').slideDown();
     });
-    $('.gnb-close').click(function(){
+    $('.gnb-close').click(function () {
         $('.mobile-nav').slideUp();
     });
-    
+
     // 언어창 토글
-    $('.lang-box').click(function(){
+    $('.lang-box').click(function () {
         $('.lang-box ul').stop().slideToggle(300);
-        });
+    });
 
     // 메인비주얼 페이드효과
-        $('.fade-banner').find('img:gt(0)').hide();
-        let current = 0;
-        setInterval(function(){
+    $('.fade-banner').find('img:gt(0)').hide();
+    let current = 0;
+    setInterval(function () {
         let next = (current + 1) % 3;
 
         $('.fade-banner').find('img').eq(current).fadeOut(0);
         $('.fade-banner').find('img').eq(next).fadeIn(800);
         console.log(current, next);
         current = next;
-        },5000);
+    }, 5000);
 
-        $(function(){
-            $(window).scroll(function(){
-                let scrollPosition = $(window).scrollTop();
-                console.log(scrollPosition);
-                if( scrollPosition >= 100 ){
-                   $('.header-slide').stop().slideDown(500);
-                } else {
-                    $('.header-slide').stop().slideUp(500);
-                }
-            });
+
+    // $(function(){
+    //     $(window).scroll(function(){
+    //         let scrollPosition = $(window).scrollTop();
+    //         console.log(scrollPosition);
+    //         if( scrollPosition >= 100 ){
+    //            $('#main-header').stop().slideDown(500);
+    //         } else {
+    //             $('#main-header').stop().slideUp(500);
+    //         }
+    //     });
+    // });
+
+    // 스크롤 업다운감지해서 헤더 숨기기
+    $(function () {
+        $(window).bind('mousewheel', function (event) {
+            if (event.originalEvent.wheelDelta >= 0) {
+                $('#main-header').slideDown();
+            } else {
+                $('#main-header').slideUp();
+            }
         });
+
     });
+});
 
